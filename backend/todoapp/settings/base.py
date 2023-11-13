acb722bfa7ad6bc0c52b403f030af98a2ee4f13d
seutil <seutil@inbox.ru>
2023-11-13 12:43:57 +0300

    Add djangorestframework and corsheaders

--- backend/todoapp/settings/base.py
+++ backend/todoapp/settings/base.py
@@ -38,11 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- backend/todoapp/settings/dev.py
+++ backend/todoapp/settings/dev.py
@@ -3,6 +3,8 @@ from .base import *
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
