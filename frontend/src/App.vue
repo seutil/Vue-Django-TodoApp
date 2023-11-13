bc6eb8f444599b52194a179c2bb671b2137a9b1c
seutil <seutil@inbox.ru>
2023-11-13 16:19:45 +0300

    Clear and include bulma

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div id="app" class="pt-5 mx-5">
     <router-view/>
   </div>
 </template>
