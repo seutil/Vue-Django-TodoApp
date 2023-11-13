1a5dcf9770097c2eaf835516060926d1036c741b
seutil <seutil@inbox.ru>
2023-11-13 13:52:25 +0300

    Add TaskViewSet

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import (viewsets as rest_viewsets,
+                            authentication as rest_authentication,
+                            permissions as rest_permissions)
 
-# Create your views here.
+from . import models as tasks_models
+from . import serializers as tasks_serializers
+
+
+class TaskViewSet(rest_viewsets.ModelViewSet):
+    queryset = tasks_models.Task.objects.all()
+    serializer_class = tasks_serializers.TaskSerializer
+    permission_classes = [rest_permissions.IsAuthenticatedOrReadOnly]
+    authentication_classes = [rest_authentication.BasicAuthentication]
